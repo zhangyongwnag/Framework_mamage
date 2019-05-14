9b673139467f92f85fe9cf59a0e5bd2ea92d5fc6
zhangyongwang <765298337@qq.com>
2019-05-14 11:53:08 +0800

    fix the particles.value

--- src/components/ParticlesJS.vue
+++ src/components/ParticlesJS.vue
@@ -16,7 +16,7 @@
         particlesJS("particles-js", {
           "particles": {
             "number": {
-              "value": 220,
+              "value": 150,
               "density": {
                 "enable": true,
                 "value_area": 800
